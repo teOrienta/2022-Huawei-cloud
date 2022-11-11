9e014df33901dd6bdd04905aff40c1ec2968f95e
José Daniel <jdsc2@cin.ufpe.br>
2022-11-11 17:17:39 -0300

    FIX: Identify page in navbar

--- frontend/src/app/home/component/navbar/navbar.component.ts
+++ frontend/src/app/home/component/navbar/navbar.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  borderSelected = 'background-color: #254E7A';
+  url = '';
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    this.url = this.router.url;
   }
-
 }
