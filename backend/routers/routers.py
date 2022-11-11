b0f850e2b2803cbc29c48351e2631398c1b378ed
André Luiz <alps2@cin.ufpe.br>
2022-11-11 19:11:20 -0300

    lister route

--- backend/routers/routers.py
+++ backend/routers/routers.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from routers import filter
+from routers import lister
 
 routers = APIRouter()
 
 routers.include_router(filter.router)
+routers.include_router(lister.router)
